6851e04af7f8d26e61aac336eb3e5cbbe179601f
RajasJadhav <rajas.jadhav2604@gmail.com>
2026-03-28 11:02:20 +0530

    removing old object pool

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -3,17 +3,5 @@ using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
-    Dictionary<GameObject, Queue<GameObject>> prefabPools = new Dictionary<GameObject, Queue<GameObject>>;
-
-    private void CreatePool(GameObject prefab , int poolSize)
-    {
-        Queue<GameObject> newPool = new Queue<GameObject>();
-
-        for(int i=0; i<poolSize; i++)
-        {
-            GameObject obj = Instantiate(prefab);
-            obj.SetActive(false);
-            newPool.Enqueue(obj);
-        }
-    }
+    
 }
